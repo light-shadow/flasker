de97809c6c08c208b1aece163e774afbf19b4735
wangxuan007 <wangxuan890@gmail.com>
2016-06-19 12:05:11 +0800

    unit tests with selenium

--- app/main/views.py
+++ app/main/views.py
@@ -8,6 +8,17 @@ from flask.ext.login import current_user, login_required
 from ..decorators import admin_required, permission_required
 
 
+@main.route('/shutdown')
+def server_shutdown():
+    if not current_app.testing:
+        abort(404)
+    shutdown = request.environ.get('werkzeug.server.shutdown')
+    if not shutdown:
+        abort(500)
+    shutdown()
+    return 'Shutting down...'
+
+
 @main.route('/', methods=['POST', 'GET'])
 def index():
     form = PostForm()
